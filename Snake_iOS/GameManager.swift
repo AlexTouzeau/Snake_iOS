894b636792e28da5b2aa134374c0f7a1ba925b4a
Alexandre Touzeau <alexandre.touzeau@etu.utc.fr>
2020-01-02 22:44:47 +0100

    First Commit / Add project Menu

---
+++ Snake_iOS/GameManager.swift
@@ -0,0 +1,9 @@
+//
+//  GameManager.swift
+//  Snake_iOS
+//
+//  Created by Alexandre Touzeau on 1/2/20.
+//  Copyright © 2020 Alexandre Touzeau. All rights reserved.
+//
+
+import Foundation
--- Snake_iOS/GameScene.swift
+++ Snake_iOS/GameScene.swift
@@ -11,79 +11,66 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    //This var must be initialized
+    var gameLogo: SKLabelNode!
+    var copyrightLogo: SKLabelNode!
+    var bestScore: SKLabelNode!
+    var playButton: SKShapeNode!
     
+    //Function called once our GameScene has loaded
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        initializeMenu()
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        //Called before each frame is rendered
+    }
+    
+    private func initializeMenu() {
+        //Initialize our gameLogo
+        gameLogo = SKLabelNode(fontNamed: "IBM 3270")
+        gameLogo.zPosition = 1
+        gameLogo.position = CGPoint(x: 0, y: (frame.size.height/2) - 300)
+        gameLogo.fontSize = 150
+        gameLogo.text = "Snake"
+        gameLogo.fontColor = SKColor.white
+        self.addChild(gameLogo)
+        
+        //Initiliaze our copyrightLogo
+        copyrightLogo = SKLabelNode(fontNamed: "IBM 3270")
+        copyrightLogo.zPosition = 1
+        copyrightLogo.position = CGPoint(x: 0, y: (frame.size.height/2) - 390)
+        copyrightLogo.fontSize = 50
+        copyrightLogo.text = "Alexandre Touzeau"
+        copyrightLogo.fontColor = SKColor.red
+        self.addChild(copyrightLogo)
+        
+        //Initialize our bestScore
+        bestScore = SKLabelNode(fontNamed: "IBM 3270")
+        bestScore.zPosition = 1
+        bestScore.position = CGPoint(x: 0, y: copyrightLogo.position.y - 200)
+        bestScore.fontSize = 50
+        bestScore.text = "Best Score: 0"
+        bestScore.fontColor = SKColor.white
+        self.addChild(bestScore)
+    
+        //initialize our playButton
+        playButton = SKShapeNode()
+        playButton.name = "play_button"
+        playButton.zPosition = 1
+        playButton.position = CGPoint(x: 0, y: (frame.size.height / -2) + 300)
+        playButton.fillColor = SKColor.white
+        let topCorner = CGPoint(x: -65, y: 65)
+        let bottomCorner = CGPoint(x: -65, y: -65)
+        let middle = CGPoint(x: 65, y: 0)
+        let path = CGMutablePath()
+        path.addLine(to: topCorner)
+        path.addLines(between: [topCorner, bottomCorner, middle])
+        playButton.path = path
+        self.addChild(playButton)
+        
+        
+        
+        
     }
 }
